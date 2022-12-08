f5787d76315835fbff55cf8baaf79e63ba2a2631
kajdev <zekewotring@gmail.com>
2022-12-07 15:54:30 -0900

    edge runtime

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,35 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextRequest } from "next/server";
 
-interface Data {
-  prompt: string;
-  model: string;
-  width: number;
-  height: number;
-}
+export const config = {
+  runtime: "experimental-edge",
+};
 
-interface Error {
-  error: string;
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data | Error>
-) {
-  if (req.method !== "POST") {
-    res.status(400).json({ error: "only POST requests are allowed" });
-    return;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async function (req: NextRequest) {
+  if (req.method !== "POST" || !req.body) {
+    return new Response(JSON.stringify({ error: "Invalid Request" }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
-  if (!req.body.prompt) {
-    res.status(400).json({ error: "prompt is required" });
-    return;
+  const json = await req.json();
+
+  if (!json.prompt) {
+    return new Response(JSON.stringify({ error: "Invalid Request" }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
   // make request to image api and return data
   const response = await fetch(
     `https://api.stability.ai/v1alpha/generation/${
-      req.body.model || "stable-diffusion-v1-5"
+      json.model || "stable-diffusion-v1-5"
     }/text-to-image`,
     {
       method: "POST",
@@ -41,21 +42,24 @@ export default async function handler(
       body: JSON.stringify({
         text_prompts: [
           {
-            text: req.body.prompt,
+            text: json.prompt,
             weight: 1,
           },
         ],
-        samples: req.body.count || 1,
+        samples: json.count || 1,
         sampler: "K_DPMPP_2S_ANCESTRAL",
         clip_guidance_preset: "FAST_BLUE",
-        width: req.body.width || 512,
-        height: req.body.height || 512,
+        width: json.width || 512,
+        height: json.height || 512,
       }),
     }
   );
 
-  console.log(response.headers);
-
   const data = await response.json();
-  res.status(200).json(data);
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: response.status,
+  });
 }
